00bf00d00db1f55ee37f4f9a722366493e40cd74
Ein <ein.paul@gmail.com>
2018-11-21 17:39:55 -0500

    added controller and views for profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,8 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Questions
-                        <a class="btn btn-primary float-right" href="#">
+                    <div class="card-header">Questions<br/>
+                        {{--<a class="btn btn-primary float-right" href="{{ route ('questions.create') }}">--}}
                             Create a Question
                         </a>
 
@@ -29,7 +29,7 @@
                                             <div class="card-footer">
                                                 <p class="card-text">
 
-                                                    <a class="btn btn-primary float-right" href="#">
+                                                    {{--<a class="btn btn-primary float-right" href="{{ route('questions.show', ['id' => $question->id]) }}">--}}
                                                         View
                                                     </a>
                                                 </p>
