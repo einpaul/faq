3e82e12e5830bff60f78fa5feb32e69298c0fd34
Ein <ein.paul@gmail.com>
2018-12-15 18:06:53 -0500

    added answer update and delete tests

--- tests/Unit/AnswersTest.php
+++ tests/Unit/AnswersTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use Tests\Unit\Auth;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,9 +14,11 @@ class AnswersTest extends TestCase
      *
      * @return void
      */
-    public function testSave()
+
+    /** @test */
+    public function answerSave()
     {
-        $user = $user = factory(\App\User::class)->make();
+        $user = factory(\App\User::class)->make();
         $user->save();
         $question = factory(\App\Question::class)->make();
         $question->user()->associate($user);
@@ -25,4 +28,39 @@ class AnswersTest extends TestCase
         $answer->question()->associate($question);
         $this->assertTrue($answer->save());
     }
+
+    /** @test */
+    public function answerUpdate()
+    {
+        $user = factory(\App\User::class)->create();
+        $question = factory(\App\Question::class)->create([
+            'user_id' => $user->id,
+        ]);
+        $answer = factory(\App\Answer::class)->create([
+            'question_id' => $question->id,
+            'user_id' => $user->id,
+        ]);
+        $answer->update(['body' => 'Hi, this is the new body']);
+        $this->assertTrue(true);
+        $this->assertEquals('Hi, this is the new body', $answer->body );
+
+    }
+
+    /** @test */
+    public function answerDelete()
+    {
+        $user = factory(\App\User::class)->create();
+        $question = factory(\App\Question::class)->create([
+            'user_id' => $user->id,
+        ]);
+        $answer = factory(\App\Answer::class)->create([
+            'question_id' => $question->id,
+            'user_id' => $user->id,
+        ]);
+        $answer->destroy;
+        $this->assertTrue(true);
+    }
 }
+
+
+
